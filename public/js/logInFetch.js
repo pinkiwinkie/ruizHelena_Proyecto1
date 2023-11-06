87ac880d9f95f588cf11fb40b294f4a99a5f7555
pinkiwinkie <elrumor99@gmail.com>
2023-11-06 21:14:07 +0100

    error en logInFecth.js

--- public/js/logInFetch.js
+++ public/js/logInFetch.js
@@ -2,20 +2,18 @@ var formLogin = document.getElementById("formLogin");
 
 formLogin.addEventListener("submit", function(e) {
     e.preventDefault();
-
+    console.log("hiciste click");
 
     var dates = new FormData(formLogin)
+    console.log(dates.dniCliente);
+    console.log(dates.pwd);
 
 
-    fetch('http://localhost/ruizHelena_Proyecto1/serviceCliente/clienteService.php', {
-            method: 'POST',
-            body: dates
-        })
+    fetch('http://localhost/ruizHelena_Proyecto1/serviceCliente/clienteService.php')
         .then(res => res.json())
         .then(data => {
             if (data === 'confirmado') {
                 alert('Has iniciado sesión');
-                window.location.href = 'tu_pagina_de_inicio.html';
             } else {
                 alert('Inicio de sesión fallido');
             }

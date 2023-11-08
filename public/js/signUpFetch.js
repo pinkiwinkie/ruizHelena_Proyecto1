257703cd7811c570c362a6d20fde7c8fb44148a2
pinkiewinkie <elrumor99@gmail.com>
2023-11-08 16:04:30 +0100

    ahora

--- public/js/signUpFetch.js
+++ public/js/signUpFetch.js
@@ -1,31 +1,35 @@
-var formRegister = document.getElementById('formRegistro');
+var formRegister = document.getElementById("formRegistro");
 
-formRegister.addEventListener('submit', function(e) {
+formRegister.addEventListener("submit", function(e) {
     e.preventDefault();
     // console.log("hiciste click");
 
     var dates = new FormData(formRegister);
     // console.log(dates);
 
-    if (dates.get('dniCliente') == "" ||
-        dates.get('nombre') == "" ||
-        dates.get('direccion') == "" ||
-        dates.get('email') == "" ||
-        dates.get('pwd') == "") {
-        alert("Introduce valores")
+    if (
+        dates.get("dniCliente") == "" ||
+        dates.get("nombre") == "" ||
+        dates.get("direccion") == "" ||
+        dates.get("email") == "" ||
+        dates.get("pwd") == ""
+    ) {
+        alert("Introduce valores");
     } else {
-        fetch('http://localhost/ruizHelena_Proyecto1/serviceCliente/clienteService.php', {
-                method: 'POST',
-                body: dates
-            })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                if (data == 'noInsertado') {
-                    alert("El usuario ya existe")
-                } else {
-                    alert("Usuario registrado correctamente")
+        fetch(
+                "http://localhost/ruizHelena_Proyecto1/serviceCliente/clienteService.php", {
+                    method: "POST",
+                    body: dates,
                 }
-            })
+            )
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                if (data == "noInsertado") {
+                    alert("El usuario ya existe");
+                } else {
+                    alert("Usuario registrado correctamente");
+                }
+            });
     }
-})
+});

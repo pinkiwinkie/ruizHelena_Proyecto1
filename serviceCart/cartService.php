92638ec7ddbd75a5172c4bcfd6659780ad85be6d
pinkiewinkie <elrumor99@gmail.com>
2023-11-08 15:57:32 +0100

    crud carrito

--- serviceCart/cartService.php
+++ serviceCart/cartService.php
@@ -2,3 +2,12 @@
 
 include "../model/Bd.php";
 include "./clases/Cart.php";
+
+$base = new Bd();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $lineaCarrito = new Cart($_POST['idUnico'], $_POST['dniCliente'], $_POST['idProducto'], $_POST['cantidad']);
+    $lineaCarrito->insertar($base->link);
+    echo json_encode('insertado');
+    exit();
+}
--- serviceCart/clases/Cart.php
+++ serviceCart/clases/Cart.php
@@ -1,5 +1,44 @@
 <?php
 
 class Cart{
-    
+    private $idCarrito;
+    private $idUnico;
+    private $dniCliente;
+    private $idProducto;
+    private $cantidad;
+
+    function __construct($idCarrito,$idUnico,$dniCliente="",$idProducto="",$cantidad="")
+    {
+        $this->idCarrito = $idCarrito;
+        $this->idUnico = $idUnico;
+        $this->dniCliente = $dniCliente;
+        $this->idProducto = $idProducto;
+        $this->cantidad = $cantidad;
+    }
+
+    static function getAll($link){
+        try{
+            $query = "SELECT * FROM carrito";
+            $result = $link->prepare($query);
+            $result->execute();
+            return $result;
+        }catch (PDOException $e){
+            $dato = "¡Error!: " . $e->getMessage() . "<br/>";
+			require "vistas/mensaje.php";
+			die();
+        }
+    }
+
+    function insertar($link){
+        try{
+            $query = "INSERT INTO carrito VALUES(:idUnico, :dniCliente, :idProducto, :cantidad)";
+            $result = $link->prepare($query);
+            $result->execute();
+            return $result->fetch(PDO::FETCH_ASSOC);
+        }catch (PDOException $e) {
+			$dato = "¡Error!: " . $e->getMessage() . "<br/>";
+			require "vistas/mensaje.php";
+			die();
+		}
+    }
 }

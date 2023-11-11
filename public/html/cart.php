1497e266fc32ca4dd89925fa8c941c81e4023aa1
pinkiewinkie <elrumor99@gmail.com>
2023-11-11 17:17:01 +0100

    cambiando cart

--- public/html/cart.php
+++ public/html/cart.php
@@ -1,17 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fragance</title>
+    <title>Carrito</title>
     <link rel="stylesheet" href="../css/styles.css">
     <link href="https://fonts.googleapis.com/css2?family=Irish+Grover&family=Kumbh+Sans:wght@400;700&family=Poppins&family=Roboto:wght@700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 </head>
-
 <body class="body">
     <div class="wrapper">
         <!-- Sidebar -->
@@ -86,49 +84,11 @@
                 <a href="index.php">FRAGANCE</a>
                 <div class="nameUsuario">Hola, <?php echo isset($_COOKIE['nombre']) ? $_COOKIE['nombre'] : 'Usuario'; ?></div>
             </nav>
-            <!-- More sellers -->
-            <div class="container">
-                <h1 class="title text-center py-4">Productos más vendidos</h1>
-                <div class="row">
-                    <div class="col-md-6 d-flex justify-content-center">
-                        <div class="card card-home mb-3 m-2 cb1 text-center">
-                            <div class="row g-0">
-                                <div class="col-md-4">
-                                    <img src="../img/ligthblue.png" class="img-fluid rounded-start" alt="...">
-                                </div>
-                                <div class="col-md-8">
-                                    <div class="card-body">
-                                        <h5 class="card-title mb-4">Light blue</h5>
-                                        <p class="card-text">Una fragancia florida y frutal con salida de notas frescas y refinadas (manzana Granny Smith, cedro de Sicilia y jacintos silvestres). El corazón, muy femenino, se compone de jazmín, rosa blanca y bambú. El fondo amaderado y suave está constituido por madera de cedro, ámbar y almizcles.</p>
-                                        <a href="#" class="btn btn-card">Añadir al carrito</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 d-flex justify-content-center">
-                        <div class="card card-home mb-3 m-2 cb1 text-center">
-                            <div class="row g-0">
-                                <div class="col-md-4">
-                                    <img src="../img/escale.png" class="img-fluid rounded-start" alt="...">
-                                </div>
-                                <div class="col-md-8">
-                                    <div class="card-body">
-                                        <h5 class="card-title mb-4">Escalè a portofino</h5>
-                                        <p class="card-text">Un agua hespéride que se abre con “una farándula de cítricos”. La fragancia desvela luego notas de almendra amarga, de azahar, de bayas de enebro y toques amaderados de ciprés y de cedro. Un crucero olfativo en tierra italiana.</p>
-                                        <a href="#" class="btn btn-card">Añadir al carrito</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
         </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="../js/main.js"></script>
 </body>
-
 </html>
